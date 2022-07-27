539c14a92b648da8eec085f13fa20d73de7e66d0
Ethan McHugh <ethanmchugh2024@gmail.com>
2022-07-27 10:31:43 -0500

    added geocoder lookup

---
+++ Map Hackwich/LocationManager.swift
@@ -0,0 +1,20 @@
+//
+//  LocationManager.swift
+//  Map Hackwich
+//
+//  Created by Ethan McHugh on 7/27/22.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject{
+    var locationManager = CLLocationManager()
+    var geocoder = CLGeocoder()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
